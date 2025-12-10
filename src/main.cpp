e5407c8962e1389bb383794c8c937fc0bdbde0c8
sundeepKumar <balachkumar1740@gmail.com>
2025-12-10 23:01:21 +0500

    Linklist created

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
-#include "raylib.h"
+// #include "raylib.h"
 
 int main() {
-    InitWindow(800, 600, "Raylib Test");
-    SetTargetFPS(60);
     
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Raylib is working!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
-    
-    CloseWindow();
     return 0;
 }
+//test your part and then clear this main file
