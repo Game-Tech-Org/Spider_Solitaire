c3166d25aebbbd1990a1e00e8f33382183b1f43e
sundeepKumar <balachkumar1740@gmail.com>
2025-12-19 11:32:44 +0500

    Final Working-folder-structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include "raylib.h"
-
+#include <iostream>
 int main() {
-    InitWindow(800, 600, "Raylib Test");
-    SetTargetFPS(60);
     
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Raylib is working!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
-    
-    CloseWindow();
     return 0;
 }
