2a424e01ae57ddda3e37c956076138b5faf24d51
tayyab <iamtayyabshahzad2024@gmail.com>
2025-12-23 06:22:55 -0800

    converted card to class and basic setup

--- Game.Stack/main.cpp
+++ Game.Stack/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+
+class card {
+public:
+    int value;
+    bool faceup;
+
+    card() {
+        value = 0;
+        faceup = false;
+    }
+
+    card(int v, bool f) {
+        value = v;
+        faceup = f;
+    }
+};
+
+
+stack<card> table[10];
+stack<card> stockpile;
+
 int main() {
-    
+    cout << "spider solitaire - stack version" << endl;
+    cout << "using classes and stacks" << endl;
+
+    cout << "columns created: 10" << endl;
+    cout << "stock is empty for now" << endl;
+
     return 0;
 }
