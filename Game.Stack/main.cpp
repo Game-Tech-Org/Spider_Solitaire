3fe95f5a64786ef134ff710499bfef396ba22664
tayyab <iamtayyabshahzad2024@gmail.com>
2025-12-25 09:28:54 -0800

    add a basic move attempt

--- Game.Stack/main.cpp
+++ Game.Stack/main.cpp
@@ -45,9 +45,9 @@ void shufflecards() {
 
     for (int i = 0; i < temp.size(); i++) {
         int r = rand() % temp.size();
-        card x = temp[i];
+        card t = temp[i];
         temp[i] = temp[r];
-        temp[r] = x;
+        temp[r] = t;
     }
 
     for (int i = 0; i < temp.size(); i++) {
@@ -71,34 +71,305 @@ void initialdeal() {
 }
 
 void showtable() {
-    cout << "\n--- table state ---\n";
+    cout << "\n--- table ---\n";
 
     for (int i = 0; i < 10; i++) {
-        cout << "col " << i << ": ";
-
+        cout << i << ": ";
         stack<card> temp = table[i];
 
         while (!temp.empty()) {
-            card c = temp.top();
-            temp.pop();
-
-            if (c.faceup)
-                cout << c.value << " ";
+            if (temp.top().faceup)
+                cout << temp.top().value << " ";
             else
                 cout << "X ";
+            temp.pop();
         }
         cout << endl;
     }
 }
 
-int main() {
-    cout << "spider solitaire\n";
+void movecard(int from, int to) {
+    if (table[from].empty()) {
+        cout << "nothing to move\n";
+        return;
+    }
 
+    card c = table[from].top();
+    table[from].pop();
+
+    table[to].push(c);
+
+    if (!table[from].empty()) {
+        table[from].top().faceup = true;
+    }
+}
+
+int main() {
     makecards();
     shufflecards();
     initialdeal();
+
     showtable();
 
-    cout << "\nstock left: " << stockpile.size() << endl;
+    int a, b;
+    cout << "\nmove from col: ";
+    cin >> a;
+    cout << "move to col: ";
+    cin >> b;
+
+    movecard(a, b);
+
+    showtable();
+
+    return 0;
+}
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class card {
+public:
+    int value;
+    bool faceup;
+
+    card() {
+        value = 0;
+        faceup = false;
+    }
+
+    card(int v, bool f) {
+        value = v;
+        faceup = f;
+    }
+};
+
+stack<card> table[10];
+stack<card> stockpile;
+
+void makecards() {
+    for (int s = 0; s < 8; s++) {
+        for (int v = 1; v <= 13; v++) {
+            stockpile.push(card(v, false));
+        }
+    }
+}
+
+void shufflecards() {
+    vector<card> temp;
+
+    while (!stockpile.empty()) {
+        temp.push_back(stockpile.top());
+        stockpile.pop();
+    }
+
+    srand(time(0));
+
+    for (int i = 0; i < temp.size(); i++) {
+        int r = rand() % temp.size();
+        card t = temp[i];
+        temp[i] = temp[r];
+        temp[r] = t;
+    }
+
+    for (int i = 0; i < temp.size(); i++) {
+        stockpile.push(temp[i]);
+    }
+}
+
+void initialdeal() {
+    for (int col = 0; col < 10; col++) {
+        int cards = (col < 4) ? 6 : 5;
+
+        for (int i = 0; i < cards; i++) {
+            table[col].push(stockpile.top());
+            stockpile.pop();
+        }
+
+        if (!table[col].empty()) {
+            table[col].top().faceup = true;
+        }
+    }
+}
+
+void showtable() {
+    cout << "\n--- table ---\n";
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << ": ";
+        stack<card> temp = table[i];
+
+        while (!temp.empty()) {
+            if (temp.top().faceup)
+                cout << temp.top().value << " ";
+            else
+                cout << "X ";
+            temp.pop();
+        }
+        cout << endl;
+    }
+}
+
+void movecard(int from, int to) {
+    if (table[from].empty()) {
+        cout << "nothing to move\n";
+        return;
+    }
+
+    card c = table[from].top();
+    table[from].pop();
+
+    table[to].push(c);
+
+    if (!table[from].empty()) {
+        table[from].top().faceup = true;
+    }
+}
+
+int main() {
+    makecards();
+    shufflecards();
+    initialdeal();
+
+    showtable();
+
+    int a, b;
+    cout << "\nmove from col: ";
+    cin >> a;
+    cout << "move to col: ";
+    cin >> b;
+
+    movecard(a, b);
+
+    showtable();
+
+    return 0;
+}
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class card {
+public:
+    int value;
+    bool faceup;
+
+    card() {
+        value = 0;
+        faceup = false;
+    }
+
+    card(int v, bool f) {
+        value = v;
+        faceup = f;
+    }
+};
+
+stack<card> table[10];
+stack<card> stockpile;
+
+void makecards() {
+    for (int s = 0; s < 8; s++) {
+        for (int v = 1; v <= 13; v++) {
+            stockpile.push(card(v, false));
+        }
+    }
+}
+
+void shufflecards() {
+    vector<card> temp;
+
+    while (!stockpile.empty()) {
+        temp.push_back(stockpile.top());
+        stockpile.pop();
+    }
+
+    srand(time(0));
+
+    for (int i = 0; i < temp.size(); i++) {
+        int r = rand() % temp.size();
+        card t = temp[i];
+        temp[i] = temp[r];
+        temp[r] = t;
+    }
+
+    for (int i = 0; i < temp.size(); i++) {
+        stockpile.push(temp[i]);
+    }
+}
+
+void initialdeal() {
+    for (int col = 0; col < 10; col++) {
+        int cards = (col < 4) ? 6 : 5;
+
+        for (int i = 0; i < cards; i++) {
+            table[col].push(stockpile.top());
+            stockpile.pop();
+        }
+
+        if (!table[col].empty()) {
+            table[col].top().faceup = true;
+        }
+    }
+}
+
+void showtable() {
+    cout << "\n--- table ---\n";
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << ": ";
+        stack<card> temp = table[i];
+
+        while (!temp.empty()) {
+            if (temp.top().faceup)
+                cout << temp.top().value << " ";
+            else
+                cout << "X ";
+            temp.pop();
+        }
+        cout << endl;
+    }
+}
+
+void movecard(int from, int to) {
+    if (table[from].empty()) {
+        cout << "nothing to move\n";
+        return;
+    }
+
+    card c = table[from].top();
+    table[from].pop();
+
+    table[to].push(c);
+
+    if (!table[from].empty()) {
+        table[from].top().faceup = true;
+    }
+}
+
+int main() {
+    makecards();
+    shufflecards();
+    initialdeal();
+
+    showtable();
+
+    int a, b;
+    cout << "\nmove from col: ";
+    cin >> a;
+    cout << "move to col: ";
+    cin >> b;
+
+    movecard(a, b);
+
+    showtable();
+
     return 0;
 }
