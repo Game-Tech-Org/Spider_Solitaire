9b7b68b4536119942e9c58bab0d757f98c74855c
Breera-Ijaz <bscs24f20@namal.edu.pk>
2025-12-26 22:49:30 +0500

    Implemented StockPile class for card storage

--- Game.LinkedList/src/core/StockPile.h
+++ Game.LinkedList/src/core/StockPile.h
@@ -1,1 +1,34 @@
-// Updated by breera at Fri Dec 26 22:18:51 PST 2025
+#pragma once
+#include "Linklist.h"
+#include "Card.h"
+
+class StockPile {
+private:
+    LinkedList<Card> cards;
+
+public:
+    StockPile() {}
+
+    void addCard(Card c) {
+        cards.insertAtTail(c);
+    }
+
+    int size() const {
+        return cards.size();
+    }
+
+    bool isEmpty() const {
+        return cards.isEmpty();
+    }
+
+    Card drawCard() {
+        Card c = cards.getTail();
+        cards.deleteFromTail();
+        return c;
+    }
+
+    void display() const {
+        cout << "Stock: " << cards.size() / 10 << " deals remaining" << endl;
+    }
+};// Updated by breera at Fri Dec 26 22:18:51 PST 2025
+"// Updated by breera - $(date)" 
