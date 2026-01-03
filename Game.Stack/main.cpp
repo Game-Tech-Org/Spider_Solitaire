bb490daf8138c6b2251efd2a2e0125d675b46fe1
tayyab <iamtayyabshahzad2024@gmail.com>
2026-01-03 15:45:00 +0500

    Added move validation and improved table display

--- Game.Stack/main.cpp
+++ Game.Stack/main.cpp
@@ -1,50 +1,80 @@
 #include <iostream>
-
 #include <ctime>
-
 using namespace std;
 
 template <class t>
-class stackk {
+class stackk
+{
     int siz;
-    t *ptr;
+    t* ptr;
     int top;
 
 public:
-    stackk(int s = 200) {
+    stackk(int s = 200)
+    {
         siz = s;
         top = -1;
         ptr = new t[siz];
     }
 
-    bool isempty() { return top == -1; }
-    bool isfull() { return top == siz - 1; }
-
-    void push(t var) {
-        if (isfull()) return;
-        ptr[++top] = var;
+    bool isempty()
+    {
+        if (top == -1)
+            return true;
+        return false;
     }
 
-    t pop() {
-        if (isempty()) return ptr[0];
-        return ptr[top--];
+    bool isfull()
+    {
+        if (top == siz - 1)
+            return true;
+        return false;
     }
 
-    t Top() { return ptr[top]; }
-    int count() { return top + 1; }
+    void push(t var)
+    {
+        if (isfull())
+            return;
+
+        top = top + 1;
+        ptr[top] = var;
+    }
+
+    t pop()
+    {
+        if (isempty())
+            return ptr[0];
+
+        t val = ptr[top];
+        top = top - 1;
+        return val;
+    }
+
+    t Top()
+    {
+        return ptr[top];
+    }
+
+    int count()
+    {
+        return top + 1;
+    }
 };
 
-class card {
+class card
+{
 public:
     int value;
     bool faceup;
 
-    card() {
+    card()
+    {
         value = 0;
         faceup = false;
     }
 
-    card(int v, bool f) {
+    card(int v, bool f)
+    {
         value = v;
         faceup = f;
     }
@@ -54,116 +84,193 @@ stackk<card> table[10];
 stackk<card> stockpile(150);
 int stockcount = 0;
 
-void makecards() {
-    for (int s = 0; s < 8; s++)
-        for (int v = 1; v <= 13; v++) {
-            stockpile.push(card(v, false));
-            stockcount++;
+void makecards()
+{
+    for (int set = 0; set < 8; set++)
+    {
+        for (int val = 1; val <= 13; val++)
+        {
+            card c(val, false);
+            stockpile.push(c);
+            stockcount = stockcount + 1;
         }
+    }
 }
 
-void shufflecards() {
+void shufflecards()
+{
     card temp[150];
-    int k = 0;
+    int index = 0;
 
-    while (!stockpile.isempty()) {
-        temp[k++] = stockpile.pop();
-        stockcount--;
+    while (!stockpile.isempty())
+    {
+        temp[index] = stockpile.pop();
+        index = index + 1;
+        stockcount = stockcount - 1;
     }
 
     srand(time(0));
 
-    for (int i = 0; i < k; i++) {
-        int r = rand() % k;
-        card c = temp[i];
+    for (int i = 0; i < index; i++)
+    {
+        int r = rand() % index;
+        card swap = temp[i];
         temp[i] = temp[r];
-        temp[r] = c;
+        temp[r] = swap;
     }
 
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < index; i++)
+    {
         stockpile.push(temp[i]);
-        stockcount++;
+        stockcount = stockcount + 1;
     }
 }
 
-void initialdeal() {
-    for (int col = 0; col < 10; col++) {
-        int cards = (col < 4) ? 6 : 5;
+void initialdeal()
+{
+    for (int col = 0; col < 10; col++)
+    {
+        int dealcount;
 
-        for (int i = 0; i < cards; i++) {
-            table[col].push(stockpile.pop());
-            stockcount--;
+        if (col < 4)
+            dealcount = 6;
+        else
+            dealcount = 5;
+
+        for (int i = 0; i < dealcount; i++)
+        {
+            card c = stockpile.pop();
+            table[col].push(c);
+            stockcount = stockcount - 1;
         }
 
-        card t = table[col].pop();
-        t.faceup = true;
-        table[col].push(t);
+        card topcard = table[col].pop();
+        topcard.faceup = true;
+        table[col].push(topcard);
     }
 }
 
-void showtable() {
-    cout << "\n--- table ---\n";
-    for (int i = 0; i < 10; i++) {
-        cout << i << ": ";
+void printcard(card c)
+{
+    if (c.faceup == true)
+        cout << c.value << " ";
+    else
+        cout << "X ";
+}
+
+void showtable()
+{
+    cout << endl;
+    cout << "---------------- TABLE ----------------" << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Column " << i << " : ";
+
         stackk<card> temp = table[i];
-        while (!temp.isempty()) {
+
+        while (!temp.isempty())
+        {
             card c = temp.pop();
-            cout << (c.faceup ? to_string(c.value) : "X") << " ";
+            printcard(c);
         }
+
         cout << endl;
     }
-    cout << "stock left: " << stockcount << endl;
+
+    cout << endl;
+    cout << "Stock Remaining : " << stockcount << endl;
+    cout << endl;
 }
 
-void movecard(int from, int to) {
-    if (table[from].isempty()) return;
+bool validmove(int from, int to)
+{
+    if (table[from].isempty())
+        return false;
 
     card moving = table[from].Top();
 
-    if (!table[to].isempty()) {
+    if (!table[to].isempty())
+    {
         card dest = table[to].Top();
-        if (dest.value != moving.value + 1) {
-            cout << "invalid move\n";
-            return;
-        }
+
+        if (dest.value != moving.value + 1)
+            return false;
     }
 
-    table[to].push(table[from].pop());
+    return true;
+}
 
-    if (!table[from].isempty()) {
+void movecard(int from, int to)
+{
+    if (!validmove(from, to))
+    {
+        cout << "Invalid Move" << endl;
+        return;
+    }
+
+    card c = table[from].pop();
+    table[to].push(c);
+
+    if (!table[from].isempty())
+    {
         card t = table[from].pop();
         t.faceup = true;
         table[from].push(t);
     }
 }
 
-void dealmore() {
-    if (stockcount < 10) return;
-    for (int i = 0; i < 10; i++) {
+void dealmore()
+{
+    if (stockcount < 10)
+    {
+        cout << "Not enough cards in stock" << endl;
+        return;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
         card c = stockpile.pop();
-        stockcount--;
         c.faceup = true;
         table[i].push(c);
+        stockcount = stockcount - 1;
     }
 }
 
-int main() {
+void showmenu()
+{
+    cout << "Press m to move card" << endl;
+    cout << "Press d to deal more cards" << endl;
+    cout << "Press q to quit" << endl;
+}
+
+int main()
+{
     makecards();
     shufflecards();
     initialdeal();
 
-    char ch;
-    while (true) {
-        showtable();
-        cin >> ch;
+    char choice;
 
-        if (ch == 'm') {
+    while (true)
+    {
+        showtable();
+        showmenu();
+
+        cin >> choice;
+
+        if (choice == 'm')
+        {
             int a, b;
             cin >> a >> b;
             movecard(a, b);
-        } else if (ch == 'd') {
+        }
+        else if (choice == 'd')
+        {
             dealmore();
-        } else if (ch == 'q') {
+        }
+        else if (choice == 'q')
+        {
             break;
         }
     }
